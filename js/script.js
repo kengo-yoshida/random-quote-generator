e137ba11741141c6f78f0252ff3505f7a1815130
yoshidakengo <hasuhana1315@gmail.com>
2018-03-30 15:21:12 +0900

    配列を追加 JSのリンクを追加

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var quote = [
+    {
+        "quote": "nimit is good",
+        "spoke": "kengo"
+    },
+    {
+        "quote": "kengo is bad",
+        "spoke": "taiki"
+
+    },
+    {
+        "quote": "taiki is very good",
+        "spoke": "nimit"
+
+    }
+]
